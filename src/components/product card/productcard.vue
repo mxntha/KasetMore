6b68a8c6852a331f8471d87392f4b1d71638ad7d
mxntha <montitakr@gmail.com>
2024-04-01 05:30:36 +0700

    fixhomepage

--- src/components/product card/productcard.vue
+++ src/components/product card/productcard.vue
@@ -1,76 +1,91 @@
 <template>
-    <v-card
-      
-      class="mx-auto my-12"
-      max-width="374"
-    >
-      <template v-slot:loader="{ isActive }">
-        <v-progress-linear
-          :active="isActive"
-          color="deep-purple"
-          height="4"
-          indeterminate
-        ></v-progress-linear>
-      </template>
-  
-      <v-img
-        cover
-        height="250"
-        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-      ></v-img>
-  
-      <v-card-item>
-        <v-card-title>Cafe Badilico</v-card-title>
-  
-        <v-card-subtitle>
-          <span class="me-1">Local Favorite</span>
-  
-          <v-icon
-            color="error"
-            icon="mdi-fire-circle"
-            size="small"
-          ></v-icon>
-        </v-card-subtitle>
-      </v-card-item>
-  
-      <v-card-text>
-        <v-row
-          align="center"
-          class="mx-0"
-        >
-          <v-rating
-            :model-value="4.5"
-            color="amber"
-            density="compact"
-            half-increments
-            readonly
-            size="small"
-          ></v-rating>
-  
-          <div class="text-grey ms-4">
-            4.5 (413)
-          </div>
-        </v-row>
-  
-        <div class="my-4 text-subtitle-1">
-          $ • Italian, Cafe
-        </div>
-  
-        <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-      </v-card-text>
-  
-      <v-card-actions>
-        <v-btn
-          color="deep-purple-lighten-2"
-          variant="text"
-          
-        >
-          Reserve
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </template>
+   <v-card
 
+class="mx-auto my-12"
+max-width="374"
+>
+<template v-slot:loader="{ isActive }">
+  <v-progress-linear
+    :active="isActive"
+    color="deep-purple"
+    height="4"
+    indeterminate
+  ></v-progress-linear>
+</template>
+
+<v-img
+  cover
+  height="250"
+  src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+></v-img>
+
+<v-card-item>
+  <v-card-title>แตงโม</v-card-title>
+
+  <v-card-subtitle>
+    <span class="me-1">แนะนำ</span>
+
+    <v-icon
+      color="error"
+      icon="mdi-fire-circle"
+      size="small"
+    ></v-icon>
+  </v-card-subtitle>
+</v-card-item>
+
+<v-card-text>
+  
+  <v-row
+    align="start"
+    class="mx-0"
+  >
+  
+    <v-rating
+      :model-value="4.5"
+      color="amber"
+      density="compact"
+      half-increments
+      readonly
+      size="small"
+    ></v-rating>
+
+    <div class="text-grey ms-4">
+      4.5 (413)
+    </div>
+  </v-row>
+
+
+  <div class="my-4 text-subtitle-1">
+    สุพรรณบุรี
+  </div>
+
+  <div>พันธุ์ไทย ปลูกเอง ไม่มีสารเคมี</div>
+</v-card-text>
+
+<v-card-actions>
+  <v-btn
+    color="deep-purple-lighten-2"
+    variant="text"
+
+  >
+    เพิ่มลงตะกร้า
+  </v-btn>
+  <v-btn
+    color="deep-purple-lighten-2"
+    variant="text"
+    align = "and"
+  >
+    สั่งซื้อ
+  </v-btn>
+</v-card-actions>
+</v-card>
+
+    
+  </template>
+  
 <script lang="ts" setup>
+   
+    
+  
   
 </script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,23 +1,128 @@
 <template>
-<v-btn @click="gotoLogin">logincustomer</v-btn>
-<v-btn @click="gotoregisterFarmer">loginfarmer</v-btn>
-<ProductCardvue v-for="i in productlist">
+   
+  <v-toolbar color="purple">
+    <template v-slot:prepend>
+      <div class="text-h5">Logo</div>
+    </template>
+
+    <v-divider
+      class="ms-3"
+      inset
+      vertical
+    ></v-divider>
+
+    <v-toolbar-title>Kaset More</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+    
+    <v-divider vertical></v-divider>
+        <v-btn @click="gotoLogin">logincustomer</v-btn>
+
+      <v-divider vertical></v-divider>
+
+      <v-btn @click="gotoregisterFarmer">loginfarmer</v-btn>
+
+      <v-divider vertical></v-divider>
+
+     <v-menu>
+
+    <template v-slot:activator="{ props }">
+    
+    <v-btn icon v-bind="props">
+        <v-avatar icon="mdi-account" color="white"></v-avatar>
+    </v-btn>
+    </template>
+
+      <v-card min-width="300">
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
+            subtitle="Founder of Vuetify"
+            title="John Leider"
+          >
+            
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list>
+          <v-list-item>
+            <v-btn
+           
+                variant="text"
+               
+              >
+                บัญชี
+              </v-btn>
+          </v-list-item>
+          <v-btn
+          
+                variant="text"
+                
+              >
+                  ศูนย์ช่วยเหลือ
+              </v-btn>
+          <v-list-item>
+            
+          </v-list-item>
+        </v-list>
+
+        
+      </v-card>
+    </v-menu>
+  
+    
+      
+    </v-toolbar-items>
+
+    <v-divider vertical></v-divider>
+
+    
+  </v-toolbar>
+
+  <ProductCardvue v-for="i in productlist">
     
 </ProductCardvue>
+
+<div class="text-center">
     
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="8">
+          <v-container class="max-width">
+            <v-pagination
+              
+              :length="15"
+              class="my-4"
+            ></v-pagination>
+          </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 
 </template>
 
+
 <script lang="ts" setup>
+
 import { useRouter, useRoute } from 'vue-router'
 import {ProductCard} from '@/components/product card/interface'
 import ProductCardvue from "@/components/product card/productcard.vue"
 import { ref } from 'vue'
 
-const productlist = ref(["atip","kuy","boring","atippa","sudsud"])
+const productlist = ref(["atip","ff"])
 
 const router = useRouter()
     
 function gotoLogin(){router.push({path:"/login" })}
 function gotoregisterFarmer(){router.push({path:"/registerFarmer" })}
+
+
 </script>
+
