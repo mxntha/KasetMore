a57cf5a56908b0ba42736c34d8a73b4540f4778a
atippaz <nasakun13201@gmail.com>
2024-04-29 16:15:40 +0700

    format code

--- src/components/product card/interface.ts
+++ src/components/product card/interface.ts
@@ -1,10 +1,10 @@
-export interface ProductCard{
-    name : string
-    price : number
-    amount : number
-    picture : string
-    id : string
-    province : string
-    rating : number
-    description : string
+export interface ProductCard {
+  name: string
+  price: number
+  amount: number
+  picture: string
+  id: string
+  province: string
+  rating: number
+  description: string
 }
--- src/pages/account/shoppingCart.vue
+++ src/pages/account/shoppingCart.vue
@@ -1,34 +1,28 @@
 <template>
-
-<v-toolbar color="purple">
+  <v-toolbar color="purple">
     <template v-slot:prepend>
       <div class="text-h5">Logo</div>
     </template>
 
-    <v-divider
-      class="ms-3"
-      inset
-      vertical
-    ></v-divider>
+    <v-divider class="ms-3" inset vertical></v-divider>
 
     <v-toolbar-title>Kaset More</v-toolbar-title>
 
-   <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
 
-   
-   <v-card
-    class="mx-auto"
-    color="purple"
-    max-width="600"
-    style="width: 1000px">
+    <v-card
+      class="mx-auto"
+      color="purple"
+      max-width="600"
+      style="width: 1000px"
+    >
     </v-card>
 
     <v-spacer></v-spacer>
 
     <v-toolbar-items>
-    
-    <v-divider vertical></v-divider>
-        <v-btn @click="">เข้า</v-btn>
+      <v-divider vertical></v-divider>
+      <v-btn @click="">เข้า</v-btn>
 
       <v-divider vertical></v-divider>
 
@@ -36,56 +30,47 @@
 
       <v-divider vertical></v-divider>
 
-     <v-menu>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn icon v-bind="props">
+            <v-avatar icon="mdi-account" color="white"></v-avatar>
+          </v-btn>
+        </template>
 
-    <template v-slot:activator="{ props }">
-    
-    <v-btn icon v-bind="props">
-        <v-avatar icon="mdi-account" color="white"></v-avatar>
-    </v-btn>
-    </template>
+        <v-card min-width="300">
+          <v-list>
+            <div class="d-flex align-center justify-space-around">
+              <v-avatar color="info">
+                <v-icon icon="mdi-account-circle"></v-icon>
+              </v-avatar>
+            </div>
+          </v-list>
 
-      <v-card min-width="300">
-        <v-list>
-        <div class="d-flex align-center justify-space-around">
-    <v-avatar color="info">
-      <v-icon icon="mdi-account-circle"></v-icon>
-    </v-avatar>
-  </div>
-        </v-list>
+          <v-divider></v-divider>
 
-        <v-divider></v-divider>
-
-        <v-list>
-          <v-list-item>
-            
+          <v-list>
+            <v-list-item>
               <v-btn variant="text" @click="gotopersonal">บัญชี</v-btn>
-            
-          </v-list-item>
-          <v-btn variant="text" @click="gotoregisterFarmer">สมัครสมาชิกเกษตรกร</v-btn>
-          <v-list-item>
-            
-          </v-list-item>
-        </v-list>
+            </v-list-item>
+            <v-btn variant="text" @click="gotoregisterFarmer"
+              >สมัครสมาชิกเกษตรกร</v-btn
+            >
+            <v-list-item> </v-list-item>
+          </v-list>
         </v-card>
-    </v-menu>
-  </v-toolbar-items>
+      </v-menu>
+    </v-toolbar-items>
 
     <v-divider vertical></v-divider>
-</v-toolbar>
-  
-<v-card flat>
+  </v-toolbar>
+
+  <v-card flat>
     <v-card-title class="d-flex align-center pe-2">
-        <v-icon 
-        class="d-flex align-center"
-        icon="mdi-cart"
-        ></v-icon>&nbsp;
-      รถเข็น
+      <v-icon class="d-flex align-center" icon="mdi-cart"></v-icon>&nbsp; รถเข็น
 
       <v-spacer></v-spacer>
-      
+
       <v-text-field
-        
         density="compact"
         label="Search"
         prepend-inner-icon="mdi-magnify"
@@ -94,22 +79,22 @@
         hide-details
         single-line
       ></v-text-field>
-    
     </v-card-title>
 
     <v-divider></v-divider>
-
-</v-card>
-
+  </v-card>
 </template>
 
 <script>
-
 import { useRouter, useRoute } from 'vue-router'
 import { ref } from 'vue'
 
 const router = useRouter()
 
-function gotoregisterFarmer(){router.push({path:"/registerFarmer"})}
-function gotopersonal(){router.push({path:"/personal"})}
+function gotoregisterFarmer() {
+  router.push({ path: '/registerFarmer' })
+}
+function gotopersonal() {
+  router.push({ path: '/personal' })
+}
 </script>
