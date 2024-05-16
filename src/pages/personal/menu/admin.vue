b7a96e047754f36c1616596311c4f156b86372ed
mxntha <montitakr@gmail.com>
2024-05-16 20:37:04 +0700

    fix error

--- src/pages/personal/menu/admin.vue
+++ src/pages/personal/menu/admin.vue
@@ -2,4 +2,8 @@
   <v-card class="ma-8" height="835"></v-card>
 </template>
 
-<script></script>
+<script lang="ts" setup>
+import { useRouter, useRoute } from 'vue-router'
+
+const router = useRouter()
+</script>
