7e04402c136096d6a385eac721c85bda83c7c2e9
atippaz <nasakun13201@gmail.com>
2024-05-24 18:54:24 +0700

    add required

--- src/pages/register/register.vue
+++ src/pages/register/register.vue
@@ -5,137 +5,140 @@
         <v-card-title class="regis"> สมัครสมาชิก{{ titleForm }} </v-card-title>
       </v-card-item>
       <v-divider class="mb-n1" />
-
       <v-card-text>
-        <div class="font-weight-bold mb-2">ข้อมูลการลงเข้าใช้ระบบ</div>
+        <v-form v-model="valid">
+          <div class="font-weight-bold mb-2">ข้อมูลการลงเข้าใช้ระบบ</div>
 
-        <v-row class="text-subtitle-1 text-medium-emphasis">
-          <v-col cols="6">
-            ชื่อบัญชีผู้ใช้
-            <v-text-field
-              v-model="registerfarmer.username"
-              label=""
-              placeholder=""
-              required
-            ></v-text-field>
-          </v-col>
-          <v-col>
-            รหัสผ่าน
-            <v-text-field
-              v-model="registerfarmer.password"
-              label="รหัสผ่านที่ใช้งาน"
-              required
-              placeholder="xxxxxx"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <div class="font-weight-bold my-2">ข้อมูลส่วนตัว</div>
-        <v-row class="text-subtitle-1 text-medium-emphasis">
-          <v-col>
-            ชื่อจริง
-            <v-text-field
-              v-model="registerfarmer.firstname"
-              placeholder="นาย/นาง/นางสาว"
-              required
-            ></v-text-field>
-          </v-col>
-          <v-col>
-            นามสกุล
-            <v-text-field
-              v-model="registerfarmer.lastname"
-              label=""
-              placeholder=""
-              required
-            ></v-text-field>
-          </v-col>
-        </v-row>
+          <v-row class="text-subtitle-1 text-medium-emphasis">
+            <v-col cols="6">
+              ชื่อบัญชีผู้ใช้
+              <v-text-field
+                v-model="registerfarmer.username"
+                label=""
+                placeholder=""
+                :rules="[ (value: any) => value?  true:'You must enter a username.']"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col>
+              รหัสผ่าน
+              <v-text-field
+                v-model="registerfarmer.password"
+                label="รหัสผ่านที่ใช้งาน"
+                required
+                :rules="[ (value: any) => value?  true:'password']"
+                placeholder="xxxxxx"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <div class="font-weight-bold my-2">ข้อมูลส่วนตัว</div>
+          <v-row class="text-subtitle-1 text-medium-emphasis">
+            <v-col>
+              ชื่อจริง
+              <v-text-field
+                v-model="registerfarmer.firstname"
+                placeholder="นาย/นาง/นางสาว"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col>
+              นามสกุล
+              <v-text-field
+                v-model="registerfarmer.lastname"
+                label=""
+                placeholder=""
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
 
-        ที่อยู่
-        <v-textarea
-          v-model="registerfarmer.address"
-          label=""
-          placeholder="บ้านเลขที่ หมู่บ้าน หมู่ ซอย ตำบล อำเภอ จังหวัด"
-          counter="50"
-          required
-        ></v-textarea>
+          ที่อยู่
+          <v-textarea
+            v-model="registerfarmer.address"
+            label=""
+            placeholder="บ้านเลขที่ หมู่บ้าน หมู่ ซอย ตำบล อำเภอ จังหวัด"
+            counter="50"
+            required
+          ></v-textarea>
 
-        <v-row>
-          <v-col v-if="isFarmer">
-            เลขบัตรประชาชน
-            <v-text-field
-              v-model="registerfarmer.idcard"
-              label="13หลัก"
-              required
-              placeholder="xxxxxxxxxxxxx"
-              counter="13"
-            ></v-text-field>
-          </v-col>
-          <v-col v-if="isFarmer">
-            เลขหลังบัตรประชาชน
-            <v-text-field
-              v-model="registerfarmer.idcardLaser"
-              label="12หลัก"
-              required
-              placeholder="xxxxxxxxxxxx"
-              counter="12"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col class="pr-16">
-            Email
-            <v-text-field
-              v-model="registerfarmer.phone"
-              label=""
-              required
-              placeholder="xxxxxxxx@gmail.com"
-              counter=""
-            ></v-text-field>
-          </v-col>
-          <v-col class="pr-16">
-            เบอร์โทรศัพท์
-            <v-text-field
-              v-model="registerfarmer.phone"
-              label="หมายเลขโทรศัพท์"
-              required
-              placeholder="08xxxxxxxx"
-              counter="10"
-            ></v-text-field>
-          </v-col>
-          <v-col>
-            <div class="file-input pt-6">
-              <input
-                type="file"
-                name="file-input"
-                id="file-input"
-                class="file-input__input"
-                @change="handleImageChange"
-                accept="image/*"
-              />
-              <label class="file-input__label" for="file-input">
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fas"
-                  data-icon="upload"
-                  class="svg-inline--fa fa-upload fa-w-16"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
+          <v-row>
+            <v-col v-if="isFarmer">
+              เลขบัตรประชาชน
+              <v-text-field
+                v-model="registerfarmer.idcard"
+                label="13หลัก"
+                required
+                placeholder="xxxxxxxxxxxxx"
+                counter="13"
+              ></v-text-field>
+            </v-col>
+            <v-col v-if="isFarmer">
+              เลขหลังบัตรประชาชน
+              <v-text-field
+                v-model="registerfarmer.idcardLaser"
+                label="12หลัก"
+                required
+                placeholder="xxxxxxxxxxxx"
+                counter="12"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="pr-16">
+              Email
+              <v-text-field
+                v-model="registerfarmer.phone"
+                label=""
+                required
+                placeholder="xxxxxxxx@gmail.com"
+                counter=""
+              ></v-text-field>
+            </v-col>
+            <v-col class="pr-16">
+              เบอร์โทรศัพท์
+              <v-text-field
+                v-model="registerfarmer.phone"
+                label="หมายเลขโทรศัพท์"
+                required
+                placeholder="08xxxxxxxx"
+                counter="10"
+              ></v-text-field>
+            </v-col>
+            <v-col>
+              <div class="file-input pt-6">
+                <input
+                  type="file"
+                  name="file-input"
+                  id="file-input"
+                  class="file-input__input"
+                  @change="handleImageChange"
+                  accept="image/*"
+                />
+                <label class="file-input__label" for="file-input">
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fas"
+                    data-icon="upload"
+                    class="svg-inline--fa fa-upload fa-w-16"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M296 384h-80c-13.3 0-24-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c12.6 12.6 3.7 34.1-14.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h136v8c0 30.9 25.1 56 56 56h80c30.9 0 56-25.1 56-56v-8h136c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"
+                    ></path>
+                  </svg>
+                  <span>เพิ่มรูปภาพของตัวเอง</span></label
                 >
-                  <path
-                    fill="currentColor"
-                    d="M296 384h-80c-13.3 0-24-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c12.6 12.6 3.7 34.1-14.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h136v8c0 30.9 25.1 56 56 56h80c30.9 0 56-25.1 56-56v-8h136c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"
-                  ></path>
-                </svg>
-                <span>เพิ่มรูปภาพของตัวเอง</span></label
-              >
-            </div>
-            <div v-if="imageUrl != ''">
-              <img :src="imageUrl" width="200" />
-            </div>
-          </v-col>
-        </v-row>
+              </div>
+              <div v-if="imageUrl != ''">
+                <img :src="imageUrl" width="200" />
+              </div>
+            </v-col>
+          </v-row>
+        </v-form>
       </v-card-text>
       <v-divider class="mt-12" />
 
@@ -145,7 +148,14 @@
         </v-btn>
         <v-spacer></v-spacer>
 
-        <v-btn color="primary" variant="text" @click="register"> ตกลง </v-btn>
+        <v-btn
+          color="primary"
+          variant="text"
+          :disabled="!valid"
+          @click="register"
+        >
+          ตกลง
+        </v-btn>
       </v-card-actions>
     </v-card>
   </div>
@@ -193,7 +203,7 @@ const userApi = useUserApi()
 const userData = ref<BaseUserInfo | null>(null)
 const router = useRouter()
 const route = useRoute()
-
+const valid = ref(false)
 const isFarmer = computed(
   () => userData.value != null && route.query.type === 'farmer'
 )
@@ -201,7 +211,7 @@ const titleForm = computed(() =>
   route.query.type === 'farmer' ? 'เกษตรกร' : 'บัญชีผู้ใช้งาน'
 )
 onMounted(async () => {
-  userData.value = await userApi.getUserInfomation()
+  userData.value = await userApi.getUserInfomation('test 1')
   if (
     !route.query.type ||
     !(route.query.type == 'farmer' || route.query.type == 'customer')
