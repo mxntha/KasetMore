e30be1616267dd010b2dab2a4070701f0d184df7
atippaz <nasakun13201@gmail.com>
2024-05-29 16:54:28 +0700

    fix path url

--- src/composables/api/baseApi.ts
+++ src/composables/api/baseApi.ts
@@ -1,28 +1,17 @@
 const baseUrl = process.env.baseUrl || 'http://localhost:5000/api/'
 
-async function getMethod<T>(
-  url: string,
-  query: object | null = null
-): Promise<T> {
+async function getMethod<T>(url: string, query: any | null = null): Promise<T> {
   console.log('get', url)
   const jwt = localStorage.getItem('login')
-  const queryString =
-    query != null ? new URLSearchParams(JSON.stringify(query)) : null
-  console.log(
-    'res',
-    baseUrl + url + queryString != null ? '?' + queryString : ''
-  )
-  console.log('ee', JSON.stringify(query))
-  return fetch(
-    baseUrl + url + queryString != null ? '?' + queryString?.toString() : '',
-    {
-      method: 'GET',
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: `Bearer ${jwt}`,
-      },
-    }
-  )
+  const queryString = query != null ? new URLSearchParams(query) : null
+  const path = baseUrl + url
+  return fetch(path + (queryString != null ? '?' + queryString : ''), {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+      Authorization: `Bearer ${jwt}`,
+    },
+  })
     .then((e) => {
       return e.json()
     })
