1ae3a0e7d209a3d93e03f2f48aa623f7530b300d
mxntha <montitakr@gmail.com>
2024-06-16 13:31:23 +0700

    update admin

--- src/pages/personal/menu/admin.vue
+++ src/pages/personal/menu/admin.vue
@@ -9,13 +9,13 @@
     <v-divider></v-divider>
     <v-card-text>
       <v-row style="height: 800px; overflow-y: auto">
-        <v-col v-for="i in userInfoData" cols="12">
+        <v-col v-for="i in sellerInfoData" cols="12">
           <v-card height="150">
             <v-row>
               <v-col cols="2">
                 <div class="d-flex justify-center ma-4">
                   <v-avatar
-                    :image="userInfoData?.profileUrl"
+                    :image="sellerInfoData?.profileUrl"
                     size="100"
                   ></v-avatar>
                 </div>
@@ -23,20 +23,20 @@
               <v-col cols="8">
                 <div class="d-flex">
                   <div class="ma-4">
-                    ชื่อ-นามสกุล : {{ userInfoData?.name }}
-                    {{ userInfoData?.lastName }}
+                    ชื่อ-นามสกุล : {{ sellerInfoData?.firstName }}
+                    {{ sellerInfoData?.lastName }}
                   </div>
-                  <div class="ma-4">Email : {{ userInfoData?.email }}</div>
+                  <div class="ma-4">Email : {{ sellerInfoData?.email }}</div>
                 </div>
                 <div class="d-flex">
                   <div class="mr-4 ml-4">
-                    เลขบัตรประชาชน : {{ userInfoData?.idCard }}
+                    เลขบัตรประชาชน : {{ sellerInfoData?.idCard }}
                   </div>
                   <div class="mr-4">
-                    เลขหลังบัตรประชาชน : {{ userInfoData?.laserCard }}
+                    เลขหลังบัตรประชาชน : {{ sellerInfoData?.laserCode }}
                   </div>
                 </div>
-                <div class="ma-4">ที่อยู่ : {{ userInfoData?.address }}</div>
+                <div class="ma-4">ที่อยู่ : {{ sellerInfoData?.address }}</div>
               </v-col>
               <v-col cols="2">
                 <div class="mt-2">
@@ -68,18 +68,40 @@
 
 <script lang="ts" setup>
 import { useRouter } from 'vue-router'
-import useUserApi, { BaseUserInfo } from '@/composables/api/useUserApi'
+import useUserApi, { SellerInfo } from '@/composables/api/useUserApi'
 import { ref, inject } from 'vue'
 import { contextPluginSymbol } from '@/plugins/context'
 
-const userInfoData = ref<BaseUserInfo | null>(null)
+const sellerInfoData = ref<SellerInfo | null>(null)
 const imageUrl = ref('')
 const router = useRouter()
 const info = inject(contextPluginSymbol)!
 const userApi = useUserApi()
 ;(async () => {
-  userInfoData.value = await userApi.getUserInfomation(
-    info.userInfomation.value?.email || ''
-  )
+  const userType = sellerInfoData.value?.userType || 'Seller'
+  const statusType = sellerInfoData.value?.statusType || 'P'
+
+  sellerInfoData.value = (await userApi.userByUserType(
+    userType,
+    statusType
+  )) || {
+    email: '',
+    firstName: '',
+    lastName: '',
+    profileUrl: '',
+    userType: '',
+    address: '',
+    displayName: '',
+    password: '',
+    statusType: '',
+    laserCode: '',
+    phoneNumber: '',
+    idCard: '',
+    createBy: '',
+    createDate: '',
+    updateBy: '',
+    updateDate: '',
+    products: [],
+  }
 })()
 </script>
