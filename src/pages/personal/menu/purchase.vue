6f859cfec27f19ba78b8ae7011e288e6b1867c96
mxntha <montitakr@gmail.com>
2024-06-25 23:31:23 +0700

    update purchase

--- src/pages/personal/menu/purchase.vue
+++ src/pages/personal/menu/purchase.vue
@@ -1,33 +1,61 @@
 <template>
   <v-card :loading="loading" class="h-100">
     note <br />
-    ต่อ api เเล้วหรือยัง <br />
+
     หากกดที่ card จะเกิดอะไรขึ้นไหม พาไปหน้าไหนเพิ่มไหม <br />
-    ใน card ui เรียบร้อยหร่ือยัง <br />
 
     ปุ่มกดได้่ทุกอันไหม
-    {{ purchaseData }}
+
     <v-card-title>ประวัติการซื้อ</v-card-title>
     <v-divider></v-divider>
     <v-card-text>
+      <!-- Render purchases if available -->
       <v-row v-if="purchaseData.length > 0">
-        <v-col v-for="i in purchaseData" :key="i.transactionId" cols="12">
-          <v-card>
+        <v-col
+          v-for="transaction in purchaseData"
+          :key="transaction.transactionId"
+          cols="12"
+        >
+          <v-card @click="goToProductDetail(transaction.productId)">
             <div class="d-flex ma-2">
               <v-icon icon="mdi-store"></v-icon>
-              <div class="pl-2">ชื่อร้านค้า : {{ userDisplay.userName }}</div>
+              <div class="pl-2 font-weight-bold user-display">
+                {{ userDisplay.userName }}
+              </div>
             </div>
             <v-divider></v-divider>
-            <div class="d-flex">
-              <v-img height="100" width="50"> </v-img>
-              <div class="pt-4">ชื่อสินค้า</div>
-              {{ i.productId }}
+            <div class="d-flex align-center ma-3">
+              <div>
+                <v-img
+                  :src="getProductImage(transaction.productId)"
+                  height="150"
+                  width="200"
+                ></v-img>
+              </div>
+
+              <div class="ml-4 align-self-center">
+                <div class="product-name">
+                  {{ getProductName(transaction.productId) }}
+                </div>
+
+                <div class="mt-2 card-description">
+                  {{ getProductDescription(transaction.productId) }}
+                </div>
+              </div>
             </div>
+
             <v-divider></v-divider>
-            <div class="ma-3">รวมการสั่งซื้อ : {{ i.price * i.amount }}</div>
+
+            <div class="d-flex justify-space-between ma-5 amount-price">
+              <div>จำนวน : {{ transaction.amount }} {{ transaction.unit }}</div>
+              <div class="d-flex flex-row-reverse">
+                รวมการสั่งซื้อ: {{ transaction.price * transaction.amount }} ฿
+              </div>
+            </div>
           </v-card>
         </v-col>
       </v-row>
+      <!-- No purchases message -->
       <div class="ma-16" v-else>
         <div class="d-flex justify-space-around">
           <v-icon
@@ -50,37 +78,112 @@ import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { searchPluginSymbol } from '@/plugins/search'
 import router from '@/router'
-import { useTransactionApi, useProductApi, useUserApi } from '@/composables/api'
-import { ProductDetailById } from '@/composables/api/interface'
+import {
+  useTransactionApi,
+  useProductApi,
+  useUserApi,
+  useUnitApi,
+} from '@/composables/api'
+import { ProductDetailById, UnitApiModel } from '@/composables/api/interface'
 import { onMounted, computed, inject } from 'vue'
 import { contextPluginSymbol } from '@/plugins/context'
 import { TransectionModel } from '@/composables/api/useTransactionApi'
-const info = inject(contextPluginSymbol)!
 
+const info = inject(contextPluginSymbol)!
 const route = useRoute()
 const purchaseData = ref<TransectionModel[]>([])
 const loading = ref(true)
 const transactionApi = useTransactionApi()
-const productDetail = ref<ProductDetailById[]>([])
+const productDetails = ref<ProductDetailById[]>([])
 const productApi = useProductApi()
 const userDisplay = ref<any>(null)
 const userApi = useUserApi()
+const unitApi = useUnitApi()
+const units = ref<UnitApiModel[]>([] as { unitId: number; unitName: string }[])
+
+const getProductDetailById = async (
+  id: number
+): Promise<ProductDetailById | null> => {
+  try {
+    return await productApi.getById(id.toString())
+  } catch {
+    return null
+  }
+}
+
+const getProductName = (productId: number) => {
+  const product = productDetails.value.find(
+    (product) => product.productId === productId
+  )
+  return product ? product.productName : 'ไม่พบข้อมูลสินค้า'
+}
+
+const getProductImage = (productId: number) => {
+  const product = productDetails.value.find(
+    (product) => product.productId === productId
+  )
+  return product ? product.productImages[0].image : '' // ตั้งค่าตามโครงการ
+}
+
+const getProductDescription = (productId: number) => {
+  const product = productDetails.value.find(
+    (product) => product.productId === productId
+  )
+  if (product) {
+    return product.description.slice(0, 2000) // ตัดข้อความเหลือ 100 ตัวอักษร
+  } else {
+    return 'ไม่พบข้อมูลสินค้า'
+  }
+}
+
+const goToProductDetail = (productId: number) => {
+  router.push({ name: 'ProductDetail', params: { id: productId } })
+}
 
 onMounted(async () => {
   loading.value = true
   purchaseData.value = await transactionApi.getByBuyer(
     info.userInfomation.value?.email!
   )!
-
-  userDisplay.value = await userApi.userByEmail(
-    info.userInfomation.value!.email
+  const sellerEmail = purchaseData.value[0].sellerEmail
+  userDisplay.value = await userApi.userByEmail(sellerEmail)
+  const productIds = purchaseData.value.map(
+    (transaction) => transaction.productId
   )
-
+  const products = await Promise.all(
+    productIds.map((id) => getProductDetailById(id))
+  )
+  productDetails.value = products.filter(
+    (product) => product !== null
+  ) as ProductDetailById[]
+  units.value = await unitApi.getAll()
   loading.value = false
 })
 </script>
 <style scoped>
 .v-card {
   overflow-y: auto;
+}
+
+.card-description {
+  max-height: 150px; /* ตั้งค่าสูงสุดของข้อความที่ต้องการ */
+  overflow: hidden;
+  text-overflow: ellipsis; /* แสดง ... เมื่อข้อความเกินความสูงสูงสุด */
+  white-space: normal; /* แสดงข้อความทั้งหมด */
+  max-width: 1200px;
+}
+
+.product-name {
+  font-size: 18px; /* ตั้งค่าขนาดตัวอักษรของชื่อสินค้า */
+  font-weight: bold; /* ตั้งค่าตัวหนาของชื่อสินค้า */
+}
+
+.user-display {
+  font-size: 18px; /* ตั้งค่าขนาดตัวอักษรของชื่อสินค้า */
+  font-weight: bold; /* ตั้งค่าตัวหนาของชื่อสินค้า */
+}
+
+.amount-price {
+  font-size: 17px;
 }
 </style>
