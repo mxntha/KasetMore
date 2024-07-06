6fe534ca57375dace46069b492e31cb40afd5e05
mxntha <montitakr@gmail.com>
2024-07-06 14:04:37 +0700

    update admin

--- src/pages/personal/admin.vue
+++ src/pages/personal/admin.vue
@@ -7,6 +7,7 @@
         <v-col v-for="i in sellerInfoData" cols="12">
           <div class="scrollable-content">
             <v-card height="150" class="card-description">
+              <v-divider></v-divider>
               <v-row>
                 <v-col cols="2">
                   <div class="d-flex justify-center ma-4">
@@ -52,6 +53,7 @@
                   </div>
                 </v-col>
               </v-row>
+              <v-divider></v-divider>
             </v-card>
           </div>
         </v-col>
